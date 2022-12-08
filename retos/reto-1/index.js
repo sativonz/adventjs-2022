37315d0122abd0edac11838cef180e711cfa9d38
sativonz <fernandogarciafornes@gmail.com>
2022-12-08 19:36:59 +0100

    Actualización

---
+++ retos/reto-1/index.js
@@ -0,0 +1,9 @@
+function wrapping(gifts) {
+  return gifts.map((gift) => {
+    let wrapped = "*".repeat(gift.length + 2);
+    return `${wrapped}\n*${gift}*\n${wrapped}`;
+  });
+}
+
+module.exports = wrapping;
+
