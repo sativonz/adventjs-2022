b4f2e69a8c32c738efce583d97cc2ff3248b47b8
sativonz <fernandogarciafornes@gmail.com>
2022-12-09 08:41:57 +0100

    Reto 2

---
+++ retos/reto-2/index.js
@@ -0,0 +1,12 @@
+function countHours(year, holidays) {
+  return holidays.map(holiday => {
+    let date = new Date(`${year}/${holiday}`)
+    return(
+      [1, 2, 3, 4, 5].includes(
+        date.getDay()
+      ) ? 1 : 0
+    )
+  }).reduce((count, horaExtra) => count + horaExtra) * 2;
+}
+
+module.exports = countHours;
